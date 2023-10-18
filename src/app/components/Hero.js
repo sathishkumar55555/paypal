4ec1d294cb8f89d8730dee00d83e164d1c232b2a
sathishkumar55555 <89385932+sathishkumar55555@users.noreply.github.com>
2023-10-18 20:24:56 +0530

    Update Hero.js

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -11,36 +11,17 @@ function classNames(...classes) {
 
 export default function Hero() {
   let [categories] = useState({
-    Recent: [
+    Personal: [
       {
         id: 1,
-        title: 'Does drinking coffee make you smarter?',
-        date: '5h ago',
-        commentCount: 5,
-        shareCount: 2,
+      
       },
-      {
-        id: 2,
-        title: "So you've bought coffee... now what?",
-        date: '2h ago',
-        commentCount: 3,
-        shareCount: 2,
-      },
+     
     ],
-    Popular: [
+    Business: [
       {
         id: 1,
-        title: 'Is tech making coffee better or worse?',
-        date: 'Jan 7',
-        commentCount: 29,
-        shareCount: 16,
-      },
-      {
-        id: 2,
-        title: 'The most innovative things happening in coffee',
-        date: 'Mar 19',
-        commentCount: 24,
-        shareCount: 12,
+        
       },
     ],
 
